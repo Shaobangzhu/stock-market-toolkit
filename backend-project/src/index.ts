ffa6e61765ab983229e1822e02ffbcb9640511a2
Chaoran Lu <andyluloveusc@gmail.com>
2025-02-19 15:21:57 -0800

    update npm

--- backend-project/src/index.ts
+++ backend-project/src/index.ts
@@ -1,5 +1,5 @@
 import { API_KEY, STOCK_SYMBOLS, TIME_TAG, DATA_FILE_PATH } from "./constants";
-import StockPriceFetcher from "./stockPriceFetcher";
+import StockPriceFetcher from "./utils/stockPriceFetcher";
 
 const operation = new StockPriceFetcher(API_KEY, STOCK_SYMBOLS, TIME_TAG, DATA_FILE_PATH);
 operation.formatAndSaveStockPrices();
