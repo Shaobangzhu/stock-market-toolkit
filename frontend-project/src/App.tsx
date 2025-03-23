646902a8d4181a1cfec679a0916fbbbc0a424831
Chaoran Lu <andyluloveusc@gmail.com>
2025-03-22 18:05:27 -0700

    implement login page

--- frontend-project/src/App.tsx
+++ frontend-project/src/App.tsx
@@ -1,11 +1,40 @@
-import React from 'react';
+import React from "react";
+import { LockOutlined } from "@ant-design/icons";
+import { Button, Form, Input } from "antd";
+import "./login.css";
 
-function App() {
+const App: React.FC = () => {
+  const onFinish = (values: any) => {
+    console.log("Received values of form: ", values);
+  };
+
   return (
-    <div>
-      Hello World!
+    <div className="login-page">
+      <Form
+        name="login"
+        initialValues={{ remember: true }}
+        style={{ maxWidth: 360 }}
+        onFinish={onFinish}
+      >
+        <Form.Item
+          name="password"
+          rules={[{ required: true, message: "Please input your Password!" }]}
+        >
+          <Input
+            prefix={<LockOutlined />}
+            type="password"
+            placeholder="Password"
+          />
+        </Form.Item>
+
+        <Form.Item>
+          <Button block type="primary" htmlType="submit">
+            Log in
+          </Button>
+        </Form.Item>
+      </Form>
     </div>
   );
-}
+};
 
 export default App;
