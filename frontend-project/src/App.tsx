379c37c8c3d9c1427a90fee5aaf155248ed2e240
Chaoran Lu <andyluloveusc@gmail.com>
2025-03-22 19:13:02 -0700

    Bug Fix: cannot find module

--- frontend-project/src/App.tsx
+++ frontend-project/src/App.tsx
@@ -1,6 +1,6 @@
 import React from "react";
 import { HashRouter, Routes, Route } from 'react-router-dom';
-import LoginPage from './Views/Login';
+import LoginPage from './Views/Login/index.tsx';
 
 const App: React.FC = () => {
 
