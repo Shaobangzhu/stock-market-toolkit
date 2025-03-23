7cba64bb64c3cea6da006b8a1a1f7d0f723b3f95
Chaoran Lu <andyluloveusc@gmail.com>
2025-03-22 19:09:57 -0700

    create login views

--- frontend-project/src/App.tsx
+++ frontend-project/src/App.tsx
@@ -1,38 +1,16 @@
 import React from "react";
-import { LockOutlined } from "@ant-design/icons";
-import { Button, Form, Input } from "antd";
-import "./login.css";
+import { HashRouter, Routes, Route } from 'react-router-dom';
+import LoginPage from './Views/Login';
 
 const App: React.FC = () => {
-  const onFinish = (values: any) => {
-    console.log("Received values of form: ", values);
-  };
 
   return (
-    <div className="login-page">
-      <Form
-        name="login"
-        initialValues={{ remember: true }}
-        style={{ maxWidth: 360 }}
-        onFinish={onFinish}
-      >
-        <Form.Item
-          name="password"
-          rules={[{ required: true, message: "Please input your Password!" }]}
-        >
-          <Input
-            prefix={<LockOutlined />}
-            type="password"
-            placeholder="Password"
-          />
-        </Form.Item>
-
-        <Form.Item>
-          <Button block type="primary" htmlType="submit">
-            Log in
-          </Button>
-        </Form.Item>
-      </Form>
+    <div>
+      <HashRouter>
+        <Routes>
+          <Route path="/" element={<LoginPage />} />
+        </Routes>
+      </HashRouter>
     </div>
   );
 };
---
+++ frontend-project/src/Views/Login/index.tsx
@@ -0,0 +1,40 @@
+import React from "react";
+import { LockOutlined } from "@ant-design/icons";
+import { Button, Form, Input } from "antd";
+import "./style.css";
+
+const LoginPage: React.FC = () => {
+  const onFinish = (values: any) => {
+    console.log("Received values of form: ", values);
+  };
+
+  return (
+    <div className="login-page">
+      <Form
+        name="login"
+        initialValues={{ remember: true }}
+        style={{ maxWidth: 360 }}
+        onFinish={onFinish}
+      >
+        <Form.Item
+          name="password"
+          rules={[{ required: true, message: "Please input your Password!" }]}
+        >
+          <Input
+            prefix={<LockOutlined />}
+            type="password"
+            placeholder="Password"
+          />
+        </Form.Item>
+
+        <Form.Item>
+          <Button block type="primary" htmlType="submit">
+            Log in
+          </Button>
+        </Form.Item>
+      </Form>
+    </div>
+  );
+};
+
+export default LoginPage;
