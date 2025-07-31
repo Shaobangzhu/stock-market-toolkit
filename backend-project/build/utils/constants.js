f0e57fbe8d1169bd58dc4476fb0cd43ae5490458
Chaoran Lu <andyluloveusc@gmail.com>
2025-07-31 14:59:13 -0700

    update info to acquire vlume for new company Figma 2

--- backend-project/build/utils/constants.js
+++ backend-project/build/utils/constants.js
@@ -5,7 +5,7 @@ var __importDefault = (this && this.__importDefault) || function (mod) {
 Object.defineProperty(exports, "__esModule", { value: true });
 exports.SINGLE_STOCK_VOLUME_DATA_FILE_PATH = exports.STOCK_PRICE_DATA_FILE_PATH = exports.TIME_TAG = exports.STOCK_SYMBOLS = exports.SINGLE_STOCK_SYMBOL = void 0;
 var path_1 = __importDefault(require("path"));
-exports.SINGLE_STOCK_SYMBOL = "OPEN";
+exports.SINGLE_STOCK_SYMBOL = "FIG";
 exports.STOCK_SYMBOLS = ["U", "PLTR", "UBER", "SOFI"];
 exports.TIME_TAG = new Date().getTime().toString();
 exports.STOCK_PRICE_DATA_FILE_PATH = path_1.default.resolve(__dirname, '../../data/prices.json');
